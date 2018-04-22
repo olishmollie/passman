7fab51ad74e26fab1b55b928f26253303267e742
ajbond <olishmollie@gmail.com>
2018-04-22 00:05:02 -0500

    Add warning on reinitialization

--- lib/init.go
+++ lib/init.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -11,6 +12,26 @@ import (
 func Init() error {
 
 	root := GetRootDir()
+	if DirExists(root) {
+		fmt.Println("Warning: password store is currently initialized.")
+		fmt.Println("Reinitialization will result in the loss of your passwords.")
+		fmt.Println("Do you wish to reinitialize? (y/N) ")
+		reader := bufio.NewReader(os.Stdin)
+		c, err := reader.ReadByte()
+		if err != nil {
+			log.Fatal("unable to read byte from stdin | ", err)
+		}
+		switch c {
+		case 'y', 'Y':
+			fmt.Println("Reinitializing...")
+			err := os.Remove(root)
+			if err != nil {
+				log.Fatal("unable to remove root dir on init | ", err)
+			}
+		default:
+			os.Exit(0)
+		}
+	}
 
 	fmt.Println("Welcome to Passman!")
 	if !DirExists(root) {
