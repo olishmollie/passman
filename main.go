6288e1da66645551030de6609a2f00e4d6ee3791
ajbond <olishmollie@gmail.com>
2018-05-16 13:49:04 -0500

    Write usage

--- main.go
+++ main.go
@@ -89,7 +89,39 @@ func checkStore() string {
 
 func checkNumArgs(num int, args []string) {
 	if len(args) != num {
-		fmt.Println("USAGE")
+		printUsage()
 		os.Exit(1)
 	}
+}
+
+func printUsage() {
+	fmt.Print(`usage: passman [opts...] [command] [args...]
+ex: passman touch Category/Website/username pswd
+commands: dump edit generate import init rm touch
+	passman - prints a tree of pswds in store
+
+    passman [opts...] <pswd_file> - prints unencrypted pswd to stdout
+		opts:
+            -copy - copies unencrypted pswd to clipboard, does not print to stdout
+
+	dump <outfile> - prints unencrypted passwords to outfile
+
+	edit <pswd_file> - opens unencrypted pswd in editor set to $VISUAL
+
+    generate [opts...] - generates a random pswd
+        opts:
+            -copy - copies unencrypted pswd to clipboard, does not print to stdout
+            -len=int - specifies length of generated pswd
+            -nosym - generate a password with no symbols
+
+    import <infile> - imports passwords from infile. infile must be newline delimited pswds,
+            e.g. Category/Website/username secret
+                 Category/AnotherWebsite/username secret2
+
+	init - create password store if it doesn't exist, and generate encryption key
+
+	rm <pswd_file> - remove <pswd_file> from pswd store
+
+	touch <pswd_file> - add <pswd_file> to pswd store
+`)
 }
