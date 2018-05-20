64ca279e7d9a8e1fe3d00374bae945cf88f480a0
ajbond <olishmollie@gmail.com>
2018-05-19 19:09:52 -0500

    Move usage to string variable in main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 
@@ -141,9 +140,44 @@ func checkNumArgs(num int, args []string) {
 }
 
 func printUsage() {
-	usage, err := ioutil.ReadFile("usage.txt")
-	if err != nil {
-		lib.FatalError(err, "could not read usage.txt")
-	}
-	fmt.Println(string(usage))
+	fmt.Println(usage)
 }
+
+var usage = `usage: passman [command] [args...] [opts...]
+
+commands: dump edit generate import init rm touch
+
+passman - prints a tree of pswds in store
+
+passman [opts...] <pswd_file> - prints unencrypted pswd
+    -c, --copy
+        copy password to clipboard
+
+dump - prints unencrypted pswds to stdout
+
+edit <pswd_file> - edit pswd in editor set to $VISUAL
+
+generate [opts...] - generates a random pswd
+    -c, --copy
+        copies unencrypted pswd to clipboard
+    -l, --len int 
+        specifies length of generated pswd
+    -n, --nosym 
+        generate a password with no symbols
+
+import <infile> - imports passwords from infile.
+    NOTE: infile must be in the following format:
+        website/username secret_password
+        Category/anothersite/username another_password
+        etc.
+
+init - create pswd store if it doesn't exist, generate encryption key
+
+lock - encrypts and dumps all passwords into one file
+
+rm <pswd_file> - remove <pswd_file> from pswd store
+
+touch <pswd_file> - add <pswd_file> to pswd store
+
+unlock - undoes lock operation
+`
