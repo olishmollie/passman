838a73f88cc930321fb3e726449f9f9b5deb405d
ajbond <olishmollie@gmail.com>
2018-06-02 17:54:31 -0500

    Trim directory name from tree command

--- passman/print.go
+++ passman/print.go
@@ -1,17 +1,30 @@
 package passman
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
 
 // Print displays all the password prefixes in dir
 func Print(dir string, offset int) {
-	cmd := exec.Command("tree", "--noreport", dir)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		FatalError(err, "could not open editor assigned to $VISUAL")
+
+	fmt.Println("Password Store")
+
+	cmd1 := exec.Command("tree", "-l", "--noreport", dir)
+	cmd2 := exec.Command("tail", "-n", "+2")
+
+	var err error
+	cmd2.Stdin, err = cmd1.StdoutPipe()
+	if err != nil {
+		FatalError(err, "unable to pipe tree into tail")
+	}
+	cmd2.Stdout = os.Stdout
+
+	err = cmd2.Start()
+	err = cmd1.Run()
+	err = cmd2.Wait()
+	if err != nil {
+		FatalError(err, "unable to print tree")
 	}
 }
