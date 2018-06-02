f8dbee4059b0834c53c9bedca6aca4eaf2385f11
ajbond <olishmollie@gmail.com>
2018-06-02 16:27:55 -0500

    Use tree for Print

--- passman/print.go
+++ passman/print.go
@@ -1,33 +1,17 @@
 package passman
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path"
-	"strings"
+	"os"
+	"os/exec"
 )
 
-// Print displays all the password prefixes in s if s is a prefix, otherwise prints password.
-func Print(s string, offset int) {
-	const lbar = "|\u2014\u2014 "
-	files, err := ioutil.ReadDir(s)
-	if err != nil {
-		FatalError(err, "could not read files in password store")
-	}
-	for _, f := range files {
-		var spaces []rune
-		for i := 0; i < offset; i++ {
-			spaces = append(spaces, ' ')
-			spaces = append(spaces, ' ')
-		}
-		n := f.Name()
-		if strings.HasPrefix(n, ".") {
-			continue
-		}
-		fmt.Println(string(spaces) + lbar + n)
-		if f.IsDir() {
-			p := path.Join(s, n)
-			Print(p, offset+2)
-		}
+// Print displays all the password prefixes in dir
+func Print(dir string, offset int) {
+	cmd := exec.Command("tree", "--noreport", dir)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		FatalError(err, "could not open editor assigned to $VISUAL")
 	}
 }
